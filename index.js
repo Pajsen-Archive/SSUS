b7d1715b0cdfd9506d1e90cdeb41971a24d41095
pajsen9263 <chaspianstoltz@icloud.com>
2022-06-13 21:01:29 +0200

    Started on the code

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const app = express();
+const {database} = require("./database.js")
+app.get("/q/:code", async function(request, response){
 
+});
 
-app.get("/r/:code", (request, response) => {});
-
-
+app.post("/add", async function(request, response){
+	let term = request.body.term;
+	let code = CreateCode(8);
+	database.save(term, code)
+})
 app.listen(3030, () => { console.log("Started server at port 3030")})
+
+
+async function CreateCode(len, callback) {
+      let text = "";
+      let text2 = "";
+      let charset =
+        "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+      for (let i = 0; i < len / 2; i++)
+        text += charset.charAt(Math.floor(Math.random() * charset.length));
+      for (let i = 0; i < len / 2; i++)
+        text2 += charset.charAt(Math.floor(Math.random() * charset.length));
+
+      callback(text + "-" + text2);
+};
