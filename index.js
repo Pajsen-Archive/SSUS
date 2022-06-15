ecee0cf56f0a46c99c218add834b7d6cccd94d69
pajsen9263 <chaspianstoltz@icloud.com>
2022-06-15 14:42:52 +0200

    Leaked my database login lol again

--- index.js
+++ index.js
@@ -2,22 +2,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser")
 const { getUrl, save, checkUnique } = require("./database.js")
-
-
-function splitString(str) {
-    const indexOfSpace = str.indexOf(' ');
-
-    if (indexOfSpace === -1) {
-        return '';
-    }
-
-    return str.substring(indexOfSpace + 1);
-}
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.get("/q/:code", async function(request, response) {
     let code = request.params.code;
     console.log(code)
@@ -31,7 +17,6 @@ app.get("/q/:code", async function(request, response) {
         response.send("URL does not exist").status(200).end()
     }
 });
-
 app.post("/add", async function(request, response) {
     let term = request.body.term;
     let code = CreateCode(8);
@@ -39,13 +24,10 @@ app.post("/add", async function(request, response) {
     response.send(code).status(200).end()
 })
 app.listen(3030, () => { console.log("Started server at port 3030") })
-
-
 async function CreateCode(len, callback) {
     let text = "";
     let text2 = "";
-    let charset =
-        "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     for (let i = 0; i < len / 2; i++)
         text += charset.charAt(Math.floor(Math.random() * charset.length));
